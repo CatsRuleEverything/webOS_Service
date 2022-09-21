faf56e7071d24194026984c9b3a1e9a83ed34902
Baek <highcloud100@inha.edu>
2022-09-21 17:41:37 +0900

    espcam 서비스 코드 추가

--- mqtts/mqttservice.js
+++ mqtts/mqttservice.js
@@ -10,13 +10,14 @@
 // eslint-disable-next-line import/no-unresolved
 const pkgInfo = require('./package.json');
 const Service = require('webos-service');
-
+const config = require('./config.json');
+const fs = require("fs");
 
 const service = new Service(pkgInfo.name); // Create service by service name on package.json
 const logHeader = "[" + pkgInfo.name + "]";
 
 const options = {
-    host: 'broker.hivemq.com',
+    host: config.mqttIP,
     port: 1883
 };
 
@@ -25,10 +26,14 @@ const options = {
 //mqtt 초기화
 const mqtt = require('mqtt');
 console.log("----initial----");
+
 const client = mqtt.connect(options);
-client.subscribe('esp/door/open');
+client.subscribe('esp/door/open'); //subscribe
+client.subscribe('espCam/img')
+
 console.log("----success----");
 
+// mqtt 저장소
 var storage = new Map();
 
 client.on('message', function(topic, msg){
@@ -44,7 +49,7 @@ service.register("getDoorOpen", function(message) {
         console.log(logHeader, "SERVICE_METHOD_CALLED:/getDoorOpen");
 
         console.log("publish");
-        client.publish('esp/door/open/get');
+        client.publish('esp/door/open/get','1');
         
         setTimeout(function(){
             if(storage.has(key)){
@@ -58,6 +63,42 @@ service.register("getDoorOpen", function(message) {
             Response: res.toString()
         });
     
+    }
+    catch{
+        console.log("error");
+    }
+  
+});
+
+
+// 배변 패드 사진 가져오기
+service.register("getToiletPicture", function(message) {
+    try{
+        var res = "";
+        var key = "espCam/img";
+        console.log(logHeader, "SERVICE_METHOD_CALLED:/getToiletPicture");
+
+        console.log("publishCam");
+        client.publish('espCam/img/get','1');
+        
+        setTimeout(function(){
+            if(storage.has(key)){
+                res = storage.get(key);
+                fs.writeFile('../mqttc/static/picture.jpeg', res, err => {
+                    if(err){
+                        console.error(err);
+                        res = false;
+                    }
+                });
+            }
+            else res = false;    
+
+
+            message.respond({
+                returnValue: true,
+                Response: true
+            });
+        },1000);
     }
     catch{
         console.log("error");
